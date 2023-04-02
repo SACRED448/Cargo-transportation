f39f6cbb9c5b5fcd0c7835c273c7f4d3606e6e48
Sergey <pavlovsergey64@gmail.com>
2023-04-02 13:40:48 +0700

    Сайт без футера

---
+++ js/index.js
@@ -0,0 +1,43 @@
+/*
+    Как рассчитываются проценты?
+    5%:
+    Перевозка стройматериалов
+    Дачный переезд
+    Грузовое такси
+    Квартирный переезд
+    3%:
+    Офисный переезд
+    Перевозка мебели
+    Перевозка вещей
+    Перевозка бытовой техники
+ */
+/*
+Тарифы:
+1ч работы такси - 300 руб
+1ч работы грузчиков - 200 руб
+ */
+
+let sum = function () {
+    let prozent = document.getElementById('3').value;
+    switch (prozent) {
+        case 'Перевозка стройматериалов':
+        case 'Дачный переезд':
+        case 'Грузовое такси':
+        case 'Квартирный переезд':
+            prozent = 1.05;
+            break;
+        case 'Офисный переезд':
+        case 'Перевозка мебели':
+        case 'Перевозка вещей':
+        case 'Перевозка бытовой техники':
+            prozent = 1.03;
+            break;
+    }
+    let totalSum = (document.getElementById('1').value * 300
+            + document.getElementById('2').value * 200) * prozent;
+    document.getElementById('4').value = totalSum;
+};
+
+
+
+
